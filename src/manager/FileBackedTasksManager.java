6b372cd35b6f535cef930fec1a30c5cc98d5a13d
Воронков Никита <nikitavoronkov@yandex.ru>
2023-04-04 12:15:02 +0300

    ФЗ6: первая итерация

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -1,15 +1,20 @@
 package manager;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Epic;
 import model.ManagerSaveException;
+import model.Status;
 import model.Subtask;
 import model.Task;
+import model.Type;
 
 import static model.Status.DONE;
 import static model.Status.NEW;
@@ -28,10 +33,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         manager.addEpic(new Epic("Epic2", "Description epic2", EPIC));
         manager.addSubtask(new Subtask("Subtask2", "Description subtask3", DONE, SUBTASK, 2));
         System.out.println(manager.getHistory());
-    }
-
-    private static TaskManager loadFromFile(File file2) {
-        return null;
     }
 
     @Override
@@ -177,13 +178,98 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     }
 
     private String toString(Task task) {
-        return null;
+        int id = task.getId();
+        Type type = task.getType();
+        String name = task.getName();
+        Status status = task.getStatus();
+        String description = task.getDescription();
+
+        if (task.getType().equals(SUBTASK)) {
+            int idEpic = ((Subtask) task).getIdEpic();
+            return String.format("%s,%s,%s,%s,%s,%s,\n", id, type, name, status, description, idEpic);
+        } else {
+            return String.format("%s,%s,%s,%s,%s,\n", id, type, name, status, description);
+        }
     }
 
-    private String historyToString(HistoryManager history) {
-        return null;
+    public static Task fromString(String value) {
+        String[] split = value.split(","); 
+        int id = Integer.parseInt(split[0]); 
+        Type type = Type.valueOf(split[1]);
+        String name = split[2];
+        Status status = Status.valueOf(split[3]);
+        String description = split[4];
+        int idEpic = 0;
+        if (split.length > 5) {
+            idEpic = Integer.parseInt(split[5]);
+        }
+        Task task = null;
+
+        switch (Type.valueOf(split[1])) {
+            case TASK:
+                task = new Task(description, name, status, type);
+                task.setId(id);
+                break;
+            case EPIC:
+                task = new Epic(description, name, type);
+                task.setId(id);
+                break;
+            case SUBTASK:
+                task = new Subtask(description, name, status, type, idEpic);
+                task.setId(id);
+                break;
+        }
+        return task;
+        
     }
 
-    
+    private static String historyToString(HistoryManager manager) {
+        StringBuilder history = new StringBuilder();
+        for (Task task : manager.getHistory()) {
+            history.append(task.getId()).append(",");
+        }
+        return history.toString();
+    }
 
+    static List<Integer> historyFromString(String value) {
+        List<Integer> history = new ArrayList<>();
+        for (String string : value.split(",")) {
+            history.add(Integer.valueOf(string));
+        }
+        return history;
+    }
+/*
+ * Подскажите, пожалуйста, в чем моя ошибка
+ * Я не очень понимаю какая логика должна быть между loadFromFile() и historyFromString()
+ */
+    public static FileBackedTasksManager loadFromFile(File file) throws ManagerSaveException {
+        FileBackedTasksManager manager = new FileBackedTasksManager(file);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            br.readLine();
+            while (br.ready()) {
+                String line = br.readLine();
+                if (!line.isEmpty()) {
+                    Task task = fromString(line);
+                    if (task != null) {
+                        switch (task.getType()) {
+                            case TASK:
+                                manager.addTask(task);
+                                break;
+                            case EPIC:
+                                manager.addEpic((Epic) task);
+                                break;
+                            case SUBTASK:
+                                manager.addSubtask((Subtask) task);
+                                break;
+                        }
+                    }
+                    historyFromString(line);
+                }
+            }
+        } catch (IOException exception) {
+            throw new ManagerSaveException("Не удалось сохранить данные.", exception);
+        }
+        manager.save();
+        return manager;
+    } 
 }
