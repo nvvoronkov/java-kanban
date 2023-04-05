ebbbdc1ca8518b7a86beb84af54dd75767a977ee
Воронков Никита <nikitavoronkov@yandex.ru>
2023-04-05 19:02:36 +0300

    fix

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -3,7 +3,7 @@ package manager;
 import java.io.File;
 
 public abstract class Manager { // Утилитарный класс, отвечающий за создание менеджера задач
-    private static File file;
+    private static File file = new File("src/model/file.csv");
 
     public static TaskManager getDefault() { // Метод, возвращающий объект-менеджер
         return new FileBackedTasksManager(file);
